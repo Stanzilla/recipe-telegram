e825b07968b320ca576fdd46453ce0ae1c852bf0
Benjamin Staneck <staneck@gmail.com>
2019-09-07 22:19:03 +0200

    properly fix notification count
    not sure why it had an exception for the search field before, removed that for now

--- webview.js
+++ webview.js
@@ -3,15 +3,10 @@ const path = require('path');
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-    let badgeCount = 0;
     const elementLoaded = document.querySelector('.header-master')
     const searchElement = document.querySelector('#header-search-inputbox');
-    const badgeText = document.querySelector('.dialog-badge-text');
-    if (badgeText) {
-      badgeCount = document.querySelector('.dialog-badge-text').textContent;
-    }
     if (elementLoaded) {
-      const elements = document.querySelectorAll('.dialog-badge:not(.jss169)');
+      const elements = document.querySelectorAll('.dialog-badge-text:not(.jss169)');
       if (elements) {
         for (let i = 0; i < elements.length; i += 1) {
           if (elements[i].innerHTML !== 0) {
@@ -21,7 +16,7 @@ module.exports = (Franz) => {
       }
     }
 
-    Franz.setBadge(badgeCount);
+    Franz.setBadge(count);
   };
 
   Franz.injectCSS(path.join(__dirname, 'service.css'));
